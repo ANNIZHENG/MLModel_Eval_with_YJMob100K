a9311e66331f418e820f99dd020907c7318fc507
Anni Zheng <annizheng3@gmail.com>
2024-06-25 12:32:32 -0400

    Update load_data.py + upload input data
    The two input data files have in total of 10k users

--- load_data.py
+++ load_data.py
@@ -3,30 +3,36 @@ import pandas as pd
 import random
 from sklearn.model_selection import train_test_split
 
-yjmob1 = 'yjmob100k-dataset1.csv.gz' # dataset under normal scenes
+# Load dataset under normal scenes
+yjmob1 = 'yjmob100k-dataset1.csv.gz'
 yjmob_df = pd.read_csv(yjmob1, compression='gzip')
-uids = yjmob_df['uid'].unique()
 
-rand_indicies = [random.randint(0, len(uids)) for _ in range(1000)]
-selected_uids = [uid for uid in uids[rand_indicies]]
-df = yjmob_df[yjmob_df['uid'].isin(selected_uids)] 
+# Retrieve all unique uids
+uids = sorted(yjmob_df['uid'].unique())
 
-# Location Data
-# linearization of the 2-dimensional grid, i.e., the original x,y coordinate system
+# Randomly select 10k users
+rand_indices = [random.randint(0, len(uids)-1) for _ in range(10000)]
+selected_uids = [uids[i] for i in rand_indices]
+df = yjmob_df[yjmob_df['uid'].isin(selected_uids)]
+
+# Create grid-like location-id via linearization
 def spatial_token(x, y):
-    # x,y are the coordinate location
-    # x determines the column order while
-    # y determines the row order
-    # (x-1) calculates the starting grid-column position
-    # (y-1)*200 calculates the start index of the grid-row
+    """
+    x: int
+    y: int
+    ret: gird-id mapped from (x,y)
+    """
     return (x-1)+(y-1)*200
-df['combined_xy'] = df.apply(lambda row: spatial_token(row['x'], row['y']), axis=1)
 
-# 8:2 split of ID
-train_uids, test_uids = train_test_split(selected_uids, test_size=0.2, random_state=42)
+df = df.copy() # this is to suppress SettingWithCopyWarning
+df['combined_xy'] = df.apply(lambda row: spatial_token(row['x'], row['y']), axis=1)
+df = df.groupby('uid').head(150)
+
+# 8:2 train-test split of uids
+train_uids, test_uids = train_test_split(selected_uids, test_size=0.2, random_state=123)
 df_train = df[df['uid'].isin(train_uids)]
 df_test = df[df['uid'].isin(test_uids)]
 
-# export data
-df_train.to_csv('df_train_1k.csv', index=False)
-df_test.to_csv('df_test_1k.csv', index=False)
+# Export data for model use
+df_train.to_csv('train.csv', index=False)
+df_test.to_csv('test.csv', index=False)
