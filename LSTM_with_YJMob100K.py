a96e1fc8411dd61c69e54096ce0c7b1a83904579
Anni Zheng <annizheng3@gmail.com>
2024-07-11 16:22:55 -0400

    Update LSTM_with_YJMob100K.py

--- LSTM_with_YJMob100K.py
+++ LSTM_with_YJMob100K.py
@@ -14,17 +14,25 @@ df_train = pd.read_csv('train.csv')
 grouped_data_train = [group for _, group in df_train.groupby('uid')]
 ## grouped_data_test  = [group for _, group in df_test.groupby('uid')]
 
-# Adjust input and predict size here
-input_size = math.floor(584 * 0.8)
-output_size = math.ceil(584 * 0.2)
+# Adjust input and output size here
+input_size  = 48 * 2 # now: 2 days # previous:  math.floor(584 * 0.8) = 467
+output_size = 48     # now: 1 day  # previous:  math.ceil(584 * 0.2) = 117
 
 class TrajectoryDataset(Dataset):
-    def __init__(self, grouped_data, input_size, predict_size):
+    def __init__(self, grouped_data, input_size, output_size):
         self.data = []
         for group in grouped_data:
             xy = group['combined_xy'].values.tolist()
             t = group['t'].values.tolist()
-            self.data.append((xy[0:input_size], xy[input_size:(input_size+predict_size)], t[0:input_size], t[input_size:(input_size+predict_size)]))
+
+            ## One-time apporach
+            # self.data.append((xy[0:input_size], xy[input_size:(input_size+output_size)], t[0:input_size], t[input_size:(input_size+output_size)]))
+            
+            # Sliding window approach
+            window_size = input_size + output_size
+            for i in range(0, len(group)-window_size+1, window_size):
+                input_end = i + input_size
+                self.data.append((xy[i:input_end], xy[input_end:(input_end+output_size)], t[i:input_end], t[input_end:(input_end+output_size)]))
 
     def __len__(self):
         return len(self.data)
@@ -185,7 +193,7 @@ def inference(model, dataloader, device, threshold=(1+math.sqrt(2))):
 
 def train_model(model, dataloader, device, epochs, learning_rate):
     for epoch in range(epochs):
-        print(f"Test: Epoch {epoch}")
+        print(f"Test: Epoch {epoch+1}")
         train(model, dataloader, device, learning_rate)
         ## print("Inference")
         ## inference(model, test_dataloader, device)
