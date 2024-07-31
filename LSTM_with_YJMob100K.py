815d46654d9a7c0d377f0b7360d4a683efd0f4e2
Anni Zheng <annizheng3@gmail.com>
2024-07-31 10:15:21 -0400

    Update LSTM_with_YJMob100K.py

--- LSTM_with_YJMob100K.py
+++ LSTM_with_YJMob100K.py
@@ -1,47 +1,30 @@
 import pandas as pd
 import numpy as np
+import math
 import torch
 import torch.nn as nn
 from torch.optim import Adam
-from torch.utils.data import Dataset, DataLoader
-import math
+from torch.utils.data import Dataset, DataLoader, Sampler
+from torch.nn.utils.rnn import pad_sequence
 
 # Load data with users from yjmob1
 df_train = pd.read_csv('train.csv')
 df_test  = pd.read_csv('test.csv')
-
-# Group data by uid
-grouped_data_train = [group for _, group in df_train.groupby('uid')]
-grouped_data_test  = [group for _, group in df_test.groupby('uid')]
+df_true_test = pd.read_csv('true_test.csv')
 
 # Adjust input and output size here
-input_size  = 48 * 2
+input_size  = 192
 output_size = 48
 
 class TrajectoryDataset(Dataset):
-    def __init__(self, grouped_data, input_size, output_size, test=False):
+    def __init__(self, all_data, input_size, output_size):
         self.data = []
-        if (test):
-            for group in grouped_data:
-                uid = group['uid'].values.tolist()
-                xy = group['combined_xy'].values.tolist()
-                t = group['t'].values.tolist()
-
-                for i in range(0, len(group), input_size):
-                    input_end = i+input_size
-                    # user_id, inputs, positions
-                    self.data.append((uid[0], xy[i:input_end], [], t[i:input_end], []))
-        else:
-            for group in grouped_data:
-                uid = group['uid'].values.tolist()
-                xy = group['combined_xy'].values.tolist()
-                t = group['t'].values.tolist()
-
-                window_size = input_size+output_size
-                for i in range(0, len(group)-window_size+1, window_size):
-                    input_end = i + input_size
-                    # user_id, inputs, labels, positions, label_positions
-                    self.data.append((uid[0], xy[i:input_end], xy[input_end:(input_end+output_size)], t[i:input_end], t[input_end:(input_end+output_size)]))
+        window_size = input_size + output_size
+        for i in range(0, len(all_data)-window_size+1, window_size):
+            uid = all_data.iloc[i]['uid']
+            xy = all_data.iloc[i:i+window_size]['combined_xy'].tolist()
+            t = all_data.iloc[i:i+window_size]['t'].tolist()
+            self.data.append((uid, xy[:input_size], xy[input_size:], t[:input_size], t[input_size:]))
 
     def __len__(self):
         return len(self.data)
@@ -50,34 +33,42 @@ class TrajectoryDataset(Dataset):
         user_id, inputs, labels, positions, label_positions = self.data[idx]
         return torch.tensor(user_id), torch.tensor(inputs), torch.tensor(labels), torch.tensor(positions), torch.tensor(label_positions)
 
-train_dataset = TrajectoryDataset(grouped_data_train, input_size, output_size, False)
-test_dataset  = TrajectoryDataset(grouped_data_test,  input_size, output_size, True)
+train_dataset = TrajectoryDataset(df_train, input_size, output_size)
+test_dataset = TrajectoryDataset(df_test, input_size, output_size)
 
-# Clutch train and test datasets into dataloaders
+class UserGroupSampler(Sampler):
+    def __init__(self, dataset):
+        self.indices_by_user = {}
+        for idx in range(len(dataset)):
+            uid, _, _, _, _ = dataset[idx]
+            uid = uid.item()
+            if uid not in self.indices_by_user:
+                self.indices_by_user[uid] = []
+            self.indices_by_user[uid].append(idx)
+
+    def __iter__(self):
+        for _, indices in self.indices_by_user.items():
+            yield indices
+
+    def __len__(self):
+        return len(self.indices_by_user)
+
 def collate_fn(batch):
-    # Unzip all batch
-    user_id, inputs_batch, labels_batch, positions_batch, label_positions_batch = zip(*batch)
+    user_ids, inputs_batch, labels_batch, positions_batch, label_positions_batch = zip(*batch)
+    inputs_padded = pad_sequence(inputs_batch, batch_first=True, padding_value=0)
+    labels_padded = pad_sequence(labels_batch, batch_first=True, padding_value=0)
+    positions_padded = pad_sequence(positions_batch, batch_first=True, padding_value=0)
+    label_positions_padded = pad_sequence(label_positions_batch, batch_first=True, padding_value=0)
+    ## return user_ids, inputs_padded, labels_padded, positions_padded, label_positions_padded
+    return user_ids[0].clone().detach(), inputs_padded, labels_padded, positions_padded, label_positions_padded
 
-    # Unpack user_id
-    unpacked_user_id = []
-    for uid in user_id:
-        unpacked_user_id.append(uid)
-    
-    # Pad the sequence with less length in a batch
-    inputs_padded = torch.nn.utils.rnn.pad_sequence(inputs_batch, padding_value=0, batch_first=True) 
-    labels_padded = torch.nn.utils.rnn.pad_sequence(labels_batch, padding_value=0, batch_first=True)
-    positions_padded = torch.nn.utils.rnn.pad_sequence(positions_batch, padding_value=0, batch_first=True) 
-    label_positions_padded = torch.nn.utils.rnn.pad_sequence(label_positions_batch, padding_value=0, batch_first=True)
-    
-    return torch.tensor(unpacked_user_id), inputs_padded, labels_padded, positions_padded, label_positions_padded
+train_sampler = UserGroupSampler(train_dataset) # group data of the same user id on the same batch
+train_dataloader = DataLoader(train_dataset, batch_sampler=train_sampler, collate_fn=collate_fn)
 
-BATCH_SIZE_train = (len(train_dataset)//len(grouped_data_train))*10
-BATCH_SIZE_test = 1 # BATCH_SIZE_train
+test_sampler = UserGroupSampler(test_dataset)
+test_dataloader = DataLoader(test_dataset, batch_sampler=test_sampler, collate_fn=collate_fn)
 
-train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE_train, shuffle=True,  collate_fn=collate_fn)
-test_dataloader  = DataLoader(test_dataset,  batch_size=BATCH_SIZE_test, shuffle=False, collate_fn=collate_fn)
-
-print(f"{len(train_dataset)} Training data and {len(test_dataset)} Testing data loaded ... with train batch size being {BATCH_SIZE_train} and with test batch size being {BATCH_SIZE_test}!")
+print("Training data and Testing data loaded!")
 
 class LSTMModel(nn.Module):
     def __init__(self, loc_size, output_size, embed_dim, hidden_size, num_layers, device):
@@ -168,155 +159,171 @@ def train(model, dataloader, device, learning_rate, threshold=(1+math.sqrt(2))):
     
     return avg_loss, avg_euclidean_distance, accuracy
 
-def inference(model, dataloader, device, threshold=(1+math.sqrt(2))):
-    model.eval() 
-    total_distance = 0.0
-    total_trajectories = 0
-    correct_trajectories = 0
-    
-    with torch.no_grad():  
-        for inputs, labels, _, _ in dataloader:
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)  
-            _, predicted = outputs.max(2)  
-            
-            # Iterate over the batch
-            for i in range(labels.size(0)):
-                true_traj = labels[i].cpu().numpy()
-                pred_traj = predicted[i].cpu().numpy()
-
-                # Decode true trajectory and predicted trajectory
-                decoded_true_traj = np.array(decode_trajectory(true_traj))
-                decoded_pred_traj = np.array(decode_trajectory(pred_traj))
-
-                # Euclidean Distance Calcualtion
-                euclidean_distances = np.linalg.norm(decoded_true_traj - decoded_pred_traj, axis=1)
-                total_distance += np.sum(euclidean_distances)
-                total_trajectories += 1
-
-                # Apply threshold
-                if euclidean_distances <= threshold:
-                    correct_trajectories += 1
-
-    # Calculate accuracy
-    avg_euclidean_distance = total_distance / total_trajectories 
-    accuracy = correct_trajectories / total_trajectories
-
-    print(f"Average Euclidean Distance Difference: {avg_euclidean_distance:.4f}, Accuracy: {accuracy:.4f}")
-
-    return accuracy
-
-def recursive_inference_per_user(model, dataloader, device, total_outputs=192):
-    model.eval()
-    all_user_predictions = {} 
-
-    with torch.no_grad():
-        start = False
-
-        for user_id, inputs, _, _, _ in dataloader:
-            
-            # Extract info from dataloader
-            user_id = user_id.item()
-            inputs = inputs.to(device)
-
-            predictions = []
-            current_input = []
-
-            if(not start):
-                current_input = inputs
-                start = True
-
-            # Generate predictions recursively for the current user
-            while len(predictions) < total_outputs:
-
-                outputs = model(current_input)
-                
-                # Get the index of the max log-probability
-                _, predicted = outputs.max(2)
-                
-                # Store prediction
-                predictions.extend(predicted.cpu().numpy().tolist()[0]) 
-
-                # Prepare for the next prediction
-                current_input = predicted
-
-            all_user_predictions[user_id] = predictions[:total_outputs]
-            start = False
-    
-    return all_user_predictions    
-
-def measure_accuracy_recursive_inference(all_user_predictions, test_data, real_test_data, threshold=(1+math.sqrt(2))):
-    total_distance = 0.0
-    total_trajectories = 0
-    correct_trajectories = 0
-
-    for test_uid, trajectory in all_user_predictions.items():
-        # Load prediction and label
-        temp_test_data = test_data[(test_data['uid']==test_uid) & (test_data['x']==999)]
-        test_data_day = temp_test_data['d']
-        test_data_time = temp_test_data['t']
-
-        temp_real_test_data = real_test_data[(real_test_data['uid']==test_uid) & (real_test_data['d'].isin(test_data_day)) & (real_test_data['t'].isin(test_data_time))]
-        predicted_test_data = trajectory[:len(temp_real_test_data)]
-        
-        # Decode true trajectory and predicted trajectory
-        decoded_true_traj = temp_real_test_data[['x', 'y']].to_numpy()
-        decoded_pred_traj = np.array(decode_trajectory(predicted_test_data))
-
-        print("True Trajectory:")
-        print(decoded_true_traj)
-        print("Predicted Trajectory:")
-        print(decoded_pred_traj)
-
-        # Euclidean Distance Calculate
-        euclidean_distances = np.linalg.norm(decoded_true_traj - decoded_pred_traj, axis=1)
-        print("Euclidean Distance Difference:", euclidean_distances)
-        print()
-
-        total_distance += np.sum(euclidean_distances)
-
-        # Apply threshold
-        for euclidean_distance in euclidean_distances:
-            if euclidean_distance <= threshold:
-                correct_trajectories += 1
-            total_trajectories += 1
-
-    # Calculate accuracy
-    avg_euclidean_distance = total_distance / total_trajectories 
-    accuracy = correct_trajectories / total_trajectories
-
-    print(f"Average Euclidean Distance Difference: {avg_euclidean_distance:.4f}, Accuracy: {accuracy:.4f}")
-    return accuracy
-
-
 def train_model(model, dataloader, device, epochs, learning_rate):
     for epoch in range(epochs):
         print(f"Test: Epoch {epoch+1}")
         train(model, dataloader, device, learning_rate)
-        ## print("Inference")
-        ## inference(model, test_dataloader, device)
 
 print("Start training process!")
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-EPOCH_NUM = 3
-model = LSTMModel(loc_size=40000, 
-                 output_size=output_size, 
-                 embed_dim=64, 
-                 hidden_size=64, 
-                 num_layers=1, 
-                 device=device)
+model = LSTMModel(loc_size=40000, output_size=output_size, embed_dim=64, hidden_size=64, num_layers=1, device=device)
 model.to(device)
-train_model(model, train_dataloader, device, EPOCH_NUM, 0.001)
+train_model(model=model, dataloader=train_dataloader, device=device, epochs=5, learning_rate=0.001)
+
+# Exapnd prediction to prepare to correspond to ground truth
+def expand_predictions(predicted_locs, predicted_times, max_time=47):
+    expanded_locs = []
+    expanded_times = list(range(max_time + 1))
+    current_loc = predicted_locs[0]
+    loc_dict = dict(zip(predicted_times, predicted_locs))
+    for time in expanded_times:
+        if time in loc_dict:
+            current_loc = loc_dict[time]
+        expanded_locs.append(current_loc)
+    return expanded_locs, expanded_times
+
+def accuracy_measure(user_id, predicted_locs, predicted_times, true_locs, true_times):
+    expanded_locs, expanded_times = expand_predictions(predicted_locs, predicted_times)
+    matched_locs = []
+
+    # Select only the prediction with a corresponding ground truth
+    for true_time in true_times:
+        if true_time in expanded_times:
+            index = expanded_times.index(true_time)
+            matched_locs.append(expanded_locs[index])
+        else:
+            matched_locs.append(None)
+
+    # Convert prediction to (x,y) trajectory
+    matched_locs = np.array(decode_trajectory(matched_locs))
+
+    # Inference
+    threshold = 1+math.sqrt(2)
+    total_distance = 0.0
+    total_location = 0
+    correct_location = 0
+
+    # Accuracy measure based on Euclidean Distance difference
+    euclidean_distances = np.linalg.norm(true_locs - matched_locs, axis=1)
+    total_distance += np.sum(euclidean_distances)
+
+    for euclidean_distance in euclidean_distances:
+        total_location += 1
+        if (euclidean_distance < threshold):
+            correct_location += 1
+
+    return matched_locs, total_distance, total_location, correct_location
+
+def recursive_inference_per_user(model, dataloader, device, true_data):
+    # Measurement used for total accuracy calculation
+    total_distances = 0.0 # total distance off
+    total_locations = 0 # total num of location to be predicted
+    correct_locations = 0 # total num of locations that are correctly predicted
+
+    model.eval()
+    
+    predictions = {} # Store the 15-day predicted values per user
+    predictions_time = {} # Store the corresponding 15-day time values per user
+    
+    with torch.no_grad():
+        for user_id, inputs, _, _, label_positions in dataloader: 
+
+            user_id = user_id.item()
+            inputs = inputs.to(device)
+            label_positions = label_positions.to(device)
+
+            # Load Ground Truth Data
+            true_data_by_uid = true_data[true_data['uid']==user_id]
+            true_locs = np.array(list(zip(true_data_by_uid['x'], true_data_by_uid['y']))) # ground truth location
+            true_times = true_data_by_uid['t'].to_list() # ground truth time
+            num_predictions = len(true_data_by_uid) # number of needed prediction
+            
+            # Store predictions and times for the current user
+            user_predictions = []
+            user_predictions_time = []
+
+            # Initial Prediction
+            outputs = model(inputs)
+            _, predicted = outputs.max(2)  # Get the index of the max log-probability
+            
+            # Set up for Autoregressive test
+            current_input = inputs
+            current_label = predicted
+            
+            for i in range(current_input.size(0)):
+                user_predictions.extend(predicted[i].cpu().numpy())
+                user_predictions_time.extend(label_positions[i].cpu().numpy())
+
+            # Autoregressive Prediction
+            while (len(user_predictions) < num_predictions):
+                # Initialize batch-related variables for prediction
+                new_inputs = []
+                new_label_positions = []
+                
+                for i in range(current_input.size(0)):
+                    true_traj = current_input[i].cpu().numpy() # len: 192
+                    pred_traj = current_label[i].cpu().numpy() # len: 48
+
+                    # Concatenate and truncate to create new input
+                    new_input_traj = torch.tensor(np.concatenate((true_traj[48:], pred_traj))).to(device) # Concatenate prediction with the previous input
+                    new_label_position = current_label_positions[i].to(device) # the KNOWN label's time data for the initial prediction
+
+                    new_inputs.append(new_input_traj)
+                    new_label_positions.append(new_label_position)
+                
+                # Stack to form a new batch
+                current_input = torch.stack(new_inputs)
+                current_label_positions = torch.stack(new_label_positions)
+                
+                # New Prediction
+                new_outputs = model(current_input) 
+                _, predicted = new_outputs.max(2)
+                
+                for i in range(current_input.size(0)):
+                    user_predictions.extend(predicted[i].cpu().numpy())
+                    user_predictions_time.extend(current_label_positions[i].cpu().numpy())
+
+                current_label = predicted
+            
+            # Measure accuracy for each user's prediction
+            matched_locs, total_distance, total_location, correct_location = accuracy_measure(user_id, user_predictions[:num_predictions], user_predictions_time[:num_predictions], true_locs, true_times)
+
+            # Store predictions and times for the user
+            predictions[user_id] = matched_locs
+            predictions_time[user_id] = true_times
+
+            # Record the total distance
+            total_distances += total_distance
+            total_locations += total_location
+            correct_locations += correct_location
+
+    avg_euclidean_distance = total_distances / total_locations
+    accuracy = correct_locations / total_locations
+
+    print(f"Total Users' Adjusted Euclidean Distance Difference: {avg_euclidean_distance:.4f}, Accuracy: {accuracy:.4f}")
+
+    return avg_euclidean_distance, accuracy, predictions, predictions_time
 
 # Autoregressive Inference
-all_user_predictions = recursive_inference_per_user(model, test_dataloader, device, total_outputs=192)
-print("Predicted data loaded!")
+print("Test")
+_, _, predictions, predictions_time = recursive_inference_per_user(model, test_dataloader, device, df_true_test)
 
-# Output accuracy
-test_data = pd.read_csv('test.csv') # file with 999 (unknown number)
-real_test_data = pd.read_csv('yjmob100k-dataset1.csv.gz', compression='gzip') # file with actual location info
-print("Actual data loaded. Ready to measure accuracy!")
+# Output data to csv
+import csv
 
-measure_accuracy_recursive_inference(all_user_predictions, test_data, real_test_data)
+# Output predicted trajectory data as CSV
+csv_data = []
+for uid in predictions:
+    locations = predictions[uid]
+    times = predictions_time[uid]
+    for time, location in zip(times, locations):
+        csv_data.append([uid, time] + location)
+
+# Write data to CSV file
+with open('lstm_prediction.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['uid', 't', 'x', 'y']) 
+    writer.writerows(csv_data)
+
+print("Predicted trajectories written to the csv file")
