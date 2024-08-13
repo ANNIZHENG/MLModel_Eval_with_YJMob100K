fef52951c4cdcd22a503a83301859426529d791a
Anni Zheng <annizheng3@gmail.com>
2024-08-13 12:25:46 -0400

    Update CustomTransformer_with_YJMob100K.py

--- CustomTransformer_with_YJMob100K.py
+++ CustomTransformer_with_YJMob100K.py
@@ -14,7 +14,7 @@ df_train = df_test
 df_true_test = pd.read_csv('true_test.csv')
 
 # Adjust input and output size here
-input_size  = 48*3
+input_size  = 48*5
 output_size = 48 
 
 class TrajectoryDataset(Dataset):
